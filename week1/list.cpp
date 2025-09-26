c71434ea4af47c9e71efa4ef0f524e498a01d242
huyduc-main <23021401@vnu.edu.vn>
2025-09-26 10:51:04 +0700

    Add files via upload

---
+++ week1/list.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+#include "list.h"
+using namespace std;
+
+void init(List& L) { L.size = 0; }
+
+int get(List& L, int i) {
+    if (i < 0 || i >= L.size) return -1;
+    return L.data[i];
+}
+
+void insertFirst(List& L, int x) {
+    for (int j = L.size; j > 0; j--)
+        L.data[j] = L.data[j - 1];
+    L.data[0] = x;
+    L.size++;
+}
+
+void insertLast(List& L, int x) {
+    L.data[L.size++] = x;
+}
+
+void insertAt(List& L, int i, int x) {
+    if (i < 0 || i > L.size) return;
+    for (int j = L.size; j > i; j--)
+        L.data[j] = L.data[j - 1];
+    L.data[i] = x;
+    L.size++;
+}
+
+void removeFirst(List& L) {
+    if (L.size == 0) return;
+    for (int j = 0; j < L.size - 1; j++)
+        L.data[j] = L.data[j + 1];
+    L.size--;
+}
+
+void removeLast(List& L) {
+    if (L.size == 0) return;
+    L.size--;
+}
+
+void removeAt(List& L, int i) {
+    if (i < 0 || i >= L.size) return;
+    for (int j = i; j < L.size - 1; j++)
+        L.data[j] = L.data[j + 1];
+    L.size--;
+}
+
+void traverseForward(List& L) {
+    for (int j = 0; j < L.size; j++)
+        cout << L.data[j] << " ";
+    cout << endl;
+}
+
+void traverseBackward(List& L) {
+    for (int j = L.size - 1; j >= 0; j--)
+        cout << L.data[j] << " ";
+    cout << endl;
+}
